5078102ebbad221e5359c52790d8253d9db654dc
NicoPut <putscher.nico-it24@it.dhbw-ravensburg.de>
2025-06-04 17:18:23 +0200

    Final 2

--- tests/test_developer.cpp
+++ tests/test_developer.cpp
@@ -4,46 +4,30 @@
 #include <gtest/gtest.h>
 #include <fstream>
 
-/**
- * @brief Test Fixture for Developer related tests.
- */
+// Test fixture for Developer-related tests
 class DeveloperTest : public ::testing::Test {
 protected:
-    JuniorDeveloper* juniorDev; ///< Pointer to a JuniorDeveloper object
-    SeniorDeveloper* seniorDev; ///< Pointer to a SeniorDeveloper object
-    const std::string logo_file_path = "test_logo.txt"; ///< Path to the test logo file
+    JuniorDeveloper* juniorDev;
+    SeniorDeveloper* seniorDev;
+    const std::string logo_file_path = "test_logo.txt";
 
-    /**
-     * @brief Sets up the test environment.
-     * Creates instances of JuniorDeveloper and SeniorDeveloper.
-     * Creates a test logo file.
-     */
     void SetUp() override {
         juniorDev = new JuniorDeveloper("Peter Parker", "Spiderman");
         seniorDev = new SeniorDeveloper("Diana Prince", "Wonder Woman");
-        
-        // Create a mock logo file for testing
+
+        // Create mock logo file
         std::ofstream outfile(logo_file_path);
         outfile << "Test Logo";
         outfile.close();
     }
 
-    /**
-     * @brief Tears down the test environment.
-     * Deletes the instances of JuniorDeveloper and SeniorDeveloper.
-     * Removes the test logo file.
-     */
     void TearDown() override {
         delete juniorDev;
         delete seniorDev;
-
-        // Remove the mock logo file
-        remove(logo_file_path.c_str());
+        std::remove(logo_file_path.c_str());
     }
 };
-/**
- * @brief Tests the constructor and getter methods of JuniorDeveloper and SeniorDeveloper.
- */
+
 TEST_F(DeveloperTest, ConstructorAndGetters) {
     EXPECT_EQ(juniorDev->get_name(), "Peter Parker");
     EXPECT_EQ(juniorDev->get_alias(), "Spiderman");
@@ -52,26 +36,16 @@ TEST_F(DeveloperTest, ConstructorAndGetters) {
     EXPECT_EQ(seniorDev->get_alias(), "Wonder Woman");
 }
 
-/**
- * @brief Tests the load_logo_from_file method of JuniorDeveloper.
- * Checks if the logo is correctly loaded from the file and verifies that an exception is thrown for an invalid file path.
- */
 TEST_F(DeveloperTest, LoadLogoFromFile) {
     EXPECT_EQ(juniorDev->get_logo(), "");
 
     juniorDev->load_logo_from_file(logo_file_path);
-    EXPECT_NE(juniorDev->get_logo(), "");
     EXPECT_EQ(juniorDev->get_logo(), "Test Logo\n");
 
     EXPECT_THROW(juniorDev->load_logo_from_file("invalid_path.txt"), std::runtime_error);
 }
 
-/**
- * @brief Main function for running all the tests.
- * @param argc Argument count
- * @param argv Argument vector
- * @return Result of running all tests
- */
+// Entry point for running all tests
 auto main(int argc, char **argv) -> int {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
